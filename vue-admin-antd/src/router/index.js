ae13ce580d29ff1b8e38014e064f850442e9b384
LYHarry <harryliubing@sina.com>
2020-05-26 23:38:38 +0800

    整理路由模块

--- vue-admin-antd/src/router/index.js
+++ vue-admin-antd/src/router/index.js
@@ -1,54 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import {
-  UserLayout,
-  BasicLayout,
-  BlankLayout
-} from '@/layouts'
+import RouteRules from './modules/route.rules'
+
+// progress bar
+import NProgress from 'nprogress'
+import storage from 'store'
+
+// import store from './store'
+
+// progress bar custom style
+import '@/components/NProgress/nprogress.less'
+
+import notification from 'ant-design-vue/es/notification'
+import { setDocumentTitle, domTitle } from '@/utils/domUtil'
+import { ACCESS_TOKEN } from '@/store/mutation-types'
+import { i18nRender } from '@/locales'
+
 
 Vue.use(VueRouter)
-
-const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import('../views/About.vue')
-  // }
-
-  {
-    path: '/user',
-    component: UserLayout,
-    redirect: '/user/login',
-    hidden: true,
-    children: [{
-        path: 'login',
-        name: 'login',
-        component: () => import('@/views/user/Login')
-      },
-      {
-        path: 'register',
-        name: 'register',
-        component: () => import('@/views/user/Register')
-      },
-      {
-        path: 'register-result',
-        name: 'registerResult',
-        component: () => import('@/views/user/RegisterResult')
-      },
-      {
-        path: 'recover',
-        name: 'recover',
-        component: undefined
-      }
-    ]
-  },
-
-]
 
 const router = new VueRouter({
   mode: 'history',
@@ -56,7 +25,83 @@ const router = new VueRouter({
   scrollBehavior: () => ({
     y: 0
   }),
-  routes
-})
+  routes: RouteRules
+});
+
+// NProgress Configuration
+NProgress.configure({ showSpinner: false })
+
+// no redirect whitelist
+const whiteList = ['login', 'register', 'registerResult']
+const loginRoutePath = '/user/login'
+const defaultRoutePath = '/dashboard/workplace'
+
+
+
+//路由拦截器
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+
+  to.meta && (typeof to.meta.title !== 'undefined' && setDocumentTitle(`${i18nRender(to.meta.title)} - ${domTitle}`))
+
+  /* has token */
+  if (storage.get(ACCESS_TOKEN)) {
+    if (to.path === loginRoutePath) {
+      next({ path: defaultRoutePath })
+      NProgress.done()
+    } else {
+      // check login user.roles is null
+      if (store.getters.roles.length === 0) {
+        // request login userInfo
+        store
+          .dispatch('GetInfo')
+          .then(res => {
+            const roles = res.result && res.result.role
+            // generate dynamic router
+            store.dispatch('GenerateRoutes', { roles }).then(() => {
+              // 根据roles权限生成可访问的路由表
+              // 动态添加可访问路由表
+              router.addRoutes(store.getters.addRouters)
+              // 请求带有 redirect 重定向时，登录自动重定向到该地址
+              const redirect = decodeURIComponent(from.query.redirect || to.path)
+              if (to.path === redirect) {
+                // set the replace: true so the navigation will not leave a history record
+                next({ ...to, replace: true })
+              } else {
+                // 跳转到目的路由
+                next({ path: redirect })
+              }
+            })
+          })
+          .catch(() => {
+            notification.error({
+              message: '错误',
+              description: '请求用户信息失败，请重试'
+            })
+            // 失败时，获取用户信息失败时，调用登出，来清空历史保留信息
+            store.dispatch('Logout').then(() => {
+              next({ path: loginRoutePath, query: { redirect: to.fullPath } })
+            })
+          })
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (whiteList.includes(to.name)) {
+      // 在免登录白名单，直接进入
+      next()
+    } else {
+      next({ path: loginRoutePath, query: { redirect: to.fullPath } })
+      NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
+    }
+  }
+
+});
+
+router.afterEach(() => {
+  NProgress.done()
+});
+
 
 export default router
---
+++ vue-admin-antd/src/router/modules/user.js
@@ -0,0 +1,29 @@
+import { UserLayout, BasicLayout, BlankLayout } from '@/components/Layouts'
+
+export default {
+    path: '/user',
+    component: UserLayout,
+    redirect: '/user/login',
+    hidden: true,
+    children: [{
+        path: 'login',
+        name: 'login',
+        component: () => import('@/views/user/Login')
+    },
+    {
+        path: 'register',
+        name: 'register',
+        component: () => import('@/views/user/Register')
+    },
+    {
+        path: 'register-result',
+        name: 'registerResult',
+        component: () => import('@/views/user/RegisterResult')
+    },
+    {
+        path: 'recover',
+        name: 'recover',
+        component: undefined
+    }
+    ]
+}
